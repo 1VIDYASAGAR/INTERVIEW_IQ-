b9949e1622dbe1a05ecdc73c6b4312feef07e136
1VIDYASAGAR <gurumandal355@gmail.com>
2026-07-15 22:42:09 +0530

    Update Google callback URL

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-     callbackURL: "http://localhost:3000/api/auth/google/callback",
+      callbackURL: "https://interview-iq-ppv9.onrender.com/api/auth/google/callback",
+    //  callbackURL: "http://localhost:3000/api/auth/google/callback",
     },
 
     async (accessToken, refreshToken, profile, done) => {
